a528d19b83bc6f4481d98397008987e534ceee42
Jochs1989 <43485024+Jochs1989@users.noreply.github.com>
2019-12-12 01:12:28 -0500

    Add files via upload

---
+++ M3HW2_Ochs/RandomNumberLibrary/RandomNumberClass.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace RandomNumberLibrary
+{
+    public class RandomNumberClass
+    {
+        public static void CreateFile(string userInput)
+        {
+            int number;
+            // This will make sure that the user entered a proper integer
+            if (int.TryParse(userInput, out number))
+            {
+                //Now if the user enters a proper integer it will use that integer and create a file.
+                try
+                {
+                    StreamWriter outputFile;
+
+                    outputFile = File.CreateText("RandomNumber.txt");
+
+                    Random rand = new Random();
+                    // The for loop will allow the program to write how ever many random numbers the user identified the file.
+                    for (int index = 0; index < number; index++)
+                    {
+                        outputFile.WriteLine(rand.Next(1, 100));
+                    }
+
+                    outputFile.Close();
+                }
+                // This is used in case the file information has an error.
+                catch
+                {
+                    Console.WriteLine("File Read Error");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number!");
+            }
+        }
+        // Used to get the information in the file the user previously created.
+        public static void ReadFile()
+        {
+            // variables to store the total for the random numbers and display the random numbers.
+            int total = 0;
+            string randomNumber;
+
+            try
+            {
+                StreamReader inputFile;
+
+                inputFile = File.OpenText("RandomNumber.txt");
+                //Used to continue until the file reaches a null point for both display of random numbers and totaling the numbers
+                while ((randomNumber = inputFile.ReadLine()) != null)
+                {
+                    Console.WriteLine(int.Parse(randomNumber));
+                    total += int.Parse(randomNumber);
+                }
+                // Displays the random numbers and total.
+                Console.WriteLine($"\n{File.ReadAllLines("RandomNumber.txt").Length} numbers were displayed and they totaled : {total}\n");
+
+                inputFile.Close();
+            }
+            // This is used in case the file information has an error.
+            catch
+            {
+                Console.WriteLine("File Read Error");
+            }
+        }
+    }
+}
---
+++ M3HW2_Ochs/RandomNumberLibrary/StandardMessages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace RandomNumberLibrary
+{
+    public class StandardMessages
+    {
+        // Allows the user to enter a number used to determine how many random numbers to show.
+        public static void GetRandomNumbers()
+        {
+            Console.Write("How many numbers would you like to see? : ");
+
+            RandomNumberClass.CreateFile(Console.ReadLine());
+            // This class method is used to display the information in the created file.
+            RandomNumberClass.ReadFile();
+        }
+        // Displays the main menu and lets the user either start or end the program.
+        public static string DisplayMenu()
+        {
+            Console.WriteLine("MENU");
+            Console.WriteLine("-----------");
+            Console.WriteLine("This program will give you a set amount of random numbers!");
+            Console.Write("Do you wish to continue? (enter 'yes' or 'no') : ");
+
+            return Console.ReadLine();
+        }
+    }
+}
