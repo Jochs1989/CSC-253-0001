beb8ccc19f964c80f263e48de5f140878472ec48
Jochs1989 <43485024+Jochs1989@users.noreply.github.com>
2019-12-11 23:47:09 -0500

    Add files via upload

---
+++ M1HW3_Ochs/CarLibrary/StandardMessages.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    public class StandardMessages
+    {
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("1. Create Car.");
+            Console.WriteLine("2. Accelerate Car.");
+            Console.WriteLine("3. Deccelerate Car.");
+            Console.WriteLine("4. Exit");
+            Console.Write("1/2/3/4 > ");
+        }
+
+        public static void DisplayMenuError()
+        {
+            Console.WriteLine("Not a valid choice!");
+            Console.WriteLine("");
+        }
+
+        public static void DisplayCarSpeed(Car input)
+        {
+            Console.WriteLine($"The {input.Year} {input.Make} is going {input.Speed} mph.");
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ M1HW3_Ochs/ConsoleUI/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+
+            Car car = new Car();
+
+            do
+            {
+                StandardMessages.DisplayMenu();
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        {
+                            car = CarAttributes.GetCarInfo();
+                            break;
+                        }
+                    case "2":
+                        {
+                            car.Accelerate();
+                            StandardMessages.DisplayCarSpeed(car);
+                            break;
+                        }
+                    case "3":
+                        {
+                            car.Brake();
+                            StandardMessages.DisplayCarSpeed(car);
+                            break;
+                        }
+                    case "4":
+                        {
+                            exit = true;
+                            break;
+                        }
+                    default:
+                        {
+                            StandardMessages.DisplayMenuError();
+                            break;
+                        }
+                }
+
+            } while (exit == false);
+        }
+    }
+}
