2b5f574e35043a33472a44cac09d09545bb0a149
Jochs1989 <43485024+Jochs1989@users.noreply.github.com>
2019-12-11 23:52:20 -0500

    Add files via upload

---
+++ M2HW1_Ochs/ConsoleUI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+*   Jeffrey Ochs
+*   CSC-253-001
+*   09-16-2019
+*   This program asks the user to enter a sentence then displays how many words are in the users sentence.
+*/
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables and do while so that the program does end until the user is done.
+            string input;
+            bool exit = false;
+            do
+            {
+                //Gets user input for the menu.
+                Console.WriteLine("Would you like to run the program (type 'yes' or 'no')");
+                input = Console.ReadLine();
+                
+                switch(input.ToLower())
+                {
+                    //calls the class method or exits in respose to users input.
+                    case "yes":
+                        DisplayResults.GetSentence();
+                        break;
+                    case "no":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid entry");
+                        break;
+                }
+                    
+
+            } while (exit == false);
+        }
+    }
+}
---
+++ M2HW1_Ochs/ConsoleUI/WordCounter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class WordCounter
+    {
+        public static int GetWordCount(string text)
+        {
+            //delims so that the string only has letters and a varibale to count the result.
+            char[] delim = {' '};
+            int wordCount = 0;
+            //for loop that counts a word after every word in a sting
+            for (int index = 0; index < text.Length; index++)
+            {
+                wordCount = text.Trim().Split(delim).Count();
+            }
+
+            return wordCount;
+        }
+    }
+}
